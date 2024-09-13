7b02bc3c733dfca877803ba5b686cfe6b0684d2d
atsa5578 <andrew.j.tsai@gmail.com>
2024-09-12 21:11:23 -0700

    Update Car.java
    car class

--- Car.java
+++ Car.java
@@ -1,1 +1,43 @@
+import java.util.Scanner;
+public class Car {
+    private String carName;
+    private int milesDriven;    
+    private double gallonsUsed;
+    
+    public Car(){
+        carName = ("");
+        milesDriven = 0;
+        gallonsUsed = 0;
+    }
+    public Car(String name, int miles, double gallons){
+        carName = name;
+        milesDriven = miles;
+        gallonsUsed = gallons;
+    }
+    public String getcarName(){
+        return carName;
+    }
+    public int getmilesDriven(){
+        return milesDriven;
+    }
+    public double getgallonsUsed(){
+        return gallonsUsed;
+    }
+    
+    public void setName (String newName){
+        newName = carName;
+    }
+    public void setMiles (int newMilesDriven){
+        newMilesDriven = milesDriven;
+    }
+    public void setGallons (double newGallons){
+        newGallons = gallonsUsed;
+    }
+    public double calculateAverage(){
+        return Math.round(milesDriven/gallonsUsed * 10)/10.0;
+    }
+    public String toString (){
+        return carName + " averaged " + calculateAverage() + " m/g ";
+    }
+    }
 
